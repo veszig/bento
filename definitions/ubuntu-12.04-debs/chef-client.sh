4b00bb2ea7d540a2c1610abb87eac1ab2911311a
Gabor Veszi <gabor.veszi@prezi.com>
2012-10-17 22:44:08 +0200

    Ubuntu definitions that install chef from the official deb packages.

---
+++ definitions/ubuntu-12.04-debs/chef-client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+echo 'deb http://apt.opscode.com/ precise-0.10 main' > \
+  /etc/apt/sources.list.d/opscode.list && wget -qO - \
+  http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add - && \
+  echo 'chef chef/chef_server_url string http://127.0.0.1:4000' | \
+  debconf-set-selections && \
+  apt-get update && apt-get -y install chef
+
